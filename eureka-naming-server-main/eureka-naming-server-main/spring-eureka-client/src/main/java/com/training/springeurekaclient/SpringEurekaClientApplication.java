854762bd16fe5a2caae75f790cbb3857879e8b64
Training4microservices <training4microservices@gmail.com>
2022-09-12 12:28:50 +0000

    Added code

---
+++ eureka-naming-server-main/eureka-naming-server-main/spring-eureka-client/src/main/java/com/training/springeurekaclient/SpringEurekaClientApplication.java
@@ -0,0 +1,31 @@
+package com.training.springeurekaclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringEurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaClientApplication.class, args);
+    }
+}
+
+@RestController
+class EurekaClientRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/clients/{applicationName}")
+    public @ResponseBody String getClientsByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName).get(0).getUri().toString();
+    }
+}
